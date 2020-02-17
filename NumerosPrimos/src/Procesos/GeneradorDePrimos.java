3b4efba3fd2c4d315fe7a2381def97484e60acf6
cristopher-silva17 <56985333+cristopher-silva17@users.noreply.github.com>
2020-02-16 22:24:36 -0500

    Add files via upload

---
+++ NumerosPrimos/src/Procesos/GeneradorDePrimos.java
@@ -0,0 +1,191 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Procesos;
+
+/**
+ *
+ * @author Cristopher Silva
+ */
+public class GeneradorDePrimos {
+//CÓDIGO DE LA TERCERA MEJORA   
+
+    private static boolean esPrimo[];
+    private static int primos[];
+
+    public static int[] generarPrimos(int max) {
+        if (max < 2) {
+            return new int[0];
+        } else {
+            inicializarCandidatos(max);
+            eliminarMultiplos();
+            obtenerCandidatosNoEliminados();
+            return primos;
+        }
+    }
+
+    private static void inicializarCandidatos(int max) {
+        int i;
+        esPrimo = new boolean[max + 1];
+        esPrimo[0] = esPrimo[1] = false;
+        for (i = 2; i < esPrimo.length; i++) {
+            esPrimo[i] = true;
+        }
+    }
+
+    private static void eliminarMultiplos() {
+        int i;
+
+        for (i = 2; i < maxFactor(); i++) {
+            if (esPrimo[i]) {
+                eliminarMultiplosDe(i);
+            }
+        }
+    }
+
+    private static int maxFactor() {
+        return (int) Math.sqrt(esPrimo.length) + 1;
+    }
+
+    private static void eliminarMultiplosDe(int i) {
+        int multiplo;
+        for (multiplo = 2 * i; multiplo < esPrimo.length; multiplo += i) {
+            esPrimo[multiplo] = false;
+        }
+    }
+
+    private static void obtenerCandidatosNoEliminados() {
+        int i, j, cuenta;
+
+        //Contar primos
+        cuenta = 0;
+        for (i = 0; i < esPrimo.length; i++) {
+            if (esPrimo[i]) {
+                cuenta++;
+            }
+        }
+
+        //Rellenar el vector de números primos 
+        primos = new int[cuenta];
+        for (i = 0, j = 0; i < esPrimo.length; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
+            }
+        }
+    }
+
+    /*
+    //CÓDIGO DE LA SEGUNDA MEJORA
+    private static boolean esPrimo[];
+    private static int primos[];
+
+    public static int[] generarPrimos(int max) {
+        if (max < 2) {
+            return new int[0]; 
+        } else {
+            inicializarCandidatos(max);
+            eliminarMultiplos();
+            obtenerCandidatosNoEliminados();
+            return primos;
+        }
+    }
+    
+    private static void inicializarCandidatos(int max){
+        int i;
+        esPrimo = new boolean[max+1];
+        esPrimo[0] = esPrimo[1] = false;
+        for(i=2;i<esPrimo.length;i++)
+            esPrimo[i] = true;
+    }
+    
+    private static void eliminarMultiplos(){
+                int i,j;
+        
+        for(i=2; i<Math.sqrt(esPrimo.length)+1;i++)
+            if(esPrimo[i]){
+                //Eliminar los múltiplos de i
+                for (j =2*i ; j < esPrimo.length; i++)
+                    esPrimo[j] = false;
+            }
+    }
+    
+    private static void obtenerCandidatosNoEliminados(){
+                int i, j, cuenta;
+
+        //Contar primos
+        cuenta = 0;
+        for (i = 0; i < esPrimo.length; i++) {
+            if (esPrimo[i]) {
+                cuenta++;
+            }
+        }
+
+        //Rellenar el vector de números primos 
+        primos = new int[cuenta];
+        for (i = 0, j = 0; i < esPrimo.length; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
+            }
+        }
+    }
+
+//CÓDIGO DE LA PRIMERA MEJORA
+    private static int dim;
+    private static boolean esPrimo[];
+    private static int primos[];
+
+    public static int[] generarPrimos(int max) {
+        if (max < 2) {
+            return new int[0]; //Vector vacío
+        } else {
+            inicializarCriba(max);
+            cribar();
+            rellenarPrimos();
+            return primos;
+        }
+    }
+
+    private static void inicializarCriba(int max) {
+        int i;
+        dim = max + 1;
+        esPrimo = new boolean[dim];
+        for (i = 0; i < dim; i++) {
+            esPrimo[i] = true;
+        }
+        esPrimo[0] = esPrimo[1] = false;
+    }
+
+    private static void cribar() {
+        int i,j;
+        
+        for(i=2; i<Math.sqrt(dim)+1;i++)
+            if(esPrimo[i]){
+                //Eliminar los múltiplos de i
+                for (j =2*i ; j < dim; i++)
+                    esPrimo[j] = false;
+            }
+    }
+
+    private static void rellenarPrimos() {
+        int i, j, cuenta;
+
+        //Contar primos
+        cuenta = 0;
+        for (i = 0; i < dim; i++) {
+            if (esPrimo[i]) {
+                cuenta++;
+            }
+        }
+
+        //Rellenar el vector de números primos 
+        primos = new int[cuenta];
+        for (i = 0, j = 0; i < dim; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
+            }
+        }
+    }
+     */
+}
---
+++ NumerosPrimos/test/Procesos/CribaTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Procesos;
+
+import junit.framework.TestCase;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ *
+ * @author Cristopher Silva
+ */
+public class CribaTest extends TestCase{
+//Programa principal (usa un componente de JUnit)
+    public static void main(String[] args) {
+        //junit.swingui.TestRunner.main(new String[] {"CribaTest"});
+        junit.textui.TestRunner.main(new String[]{"CribaTest"});
+    }
+
+    //Constructor
+    public CribaTest(String nombre) {
+        super(nombre);
+    }
+
+    //Casos de prueba
+    public void testPrimos() {
+        int[] nullArray = Criba.generarPrimos(0);
+        assertEquals(nullArray.length, 0);
+
+        int[] minArray = Criba.generarPrimos(2);
+        assertEquals(minArray.length, 1);
+        assertEquals(minArray[0], 2);
+
+        int[] threeArray = Criba.generarPrimos(3);
+        assertEquals(threeArray.length, 2);
+        assertEquals(threeArray[0], 2);
+        assertEquals(threeArray[1], 3);
+        
+        int[] centArray = Criba.generarPrimos(100);
+        assertEquals(centArray.length, 25);
+        assertEquals(centArray[24], 94);
+    }
+}
